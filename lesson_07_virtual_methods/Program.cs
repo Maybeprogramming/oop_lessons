8ac18fa0b3b9f7d15394dd393e26b603d33dc0fb
Maybeonce <a-u-rom@yandex.ru>
2023-09-11 23:31:27 +0400

    !

--- lesson_07_virtual_methods/Program.cs
+++ lesson_07_virtual_methods/Program.cs
@@ -4,6 +4,53 @@
     {
         static void Main()
         {
+            NonPlayerCharacter[] characters =
+            {
+                new NonPlayerCharacter(),
+                new Farmer(),
+                new Knight(),
+                new Child()               
+            };
+
+            foreach (NonPlayerCharacter character in characters)
+            {
+                character.ShowDescription();
+                Console.WriteLine(new string ('-', 40));
+            }
+
+            Console.ReadLine();
         }
     }
+
+    class NonPlayerCharacter
+    {
+        public virtual void ShowDescription()
+        {
+            Console.WriteLine("Я простой НПЦ, умею только гулять");
+        }
+    }
+
+    class Farmer: NonPlayerCharacter
+    {
+        public override void ShowDescription()
+        {
+            base.ShowDescription();
+            Console.WriteLine("А ещё я фермер и умею работать на поле");
+        }
+    }
+
+    class Knight : NonPlayerCharacter
+    {
+        public override void ShowDescription()
+        {
+            Console.WriteLine("Я рыцарь моё дело только сражение");
+        }
+    }
+
+    class Child : NonPlayerCharacter
+    {
+
+    }
 }
+
+//Вирутальные методы
