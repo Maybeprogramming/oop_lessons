30ecc09b12bac04a54ca26f83c4db9d54e78f0b0
Maybeonce <a-u-rom@yandex.ru>
2023-09-13 20:59:23 +0400

    Паттерн цикл обновления, полиморфизм подтипов AD HOC

--- lesson_08_update_cycle_pattern/Program.cs
+++ lesson_08_update_cycle_pattern/Program.cs
@@ -4,10 +4,47 @@
     {
         static void Main()
         {
+            Behaviour[] behaviours =
+            {
+                new Walker(),
+                new Jumper()
+            };
 
+            while (true)
+            {
+                foreach (var behaviour in behaviours)
+                {
+                    behaviour.Update();
+                    Task.Delay(500).Wait();
+                }
+            }
         }
     }
 
+    class Behaviour
+    {
+        public virtual void Update() { }
+    }
+
+    class Walker : Behaviour
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Иду");
+        }
+    }
+
+    class Jumper : Behaviour
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Прыгаю");
+        }
+    }
 }
 
+
 //Паттерн - цикл обновления
+//Примудрости полиморфизма - полиморфизм подтипов отчасти относится к "Ad hoc" полиморфизму ("специально для этого")
+//Когда мы исполняем примерно одинаковый код для разных подтипов, т.е. ->
+//У нас есть один интерфейс и множество реализаций
