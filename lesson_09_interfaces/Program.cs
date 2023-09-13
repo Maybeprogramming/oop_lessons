47a34b196c15a5b85f3a9f136681aff7ca27252d
Maybeonce <a-u-rom@yandex.ru>
2023-09-13 21:06:39 +0400

    Interface

--- lesson_09_interfaces/Program.cs
+++ lesson_09_interfaces/Program.cs
@@ -4,10 +4,57 @@
     {
         static void Main()
         {
+            IMovable car = new Car();
+            IMovable human = new Human();
+        }
+    }
+
+    interface IMovable
+    {
+        void Move();
+
+        void ShowMoveSpeed();
+    }
+
+    interface IBurnable
+    {
+        void Burn();
+    }
+
+    class Vehicle { }
+
+    class Car : Vehicle, IMovable, IBurnable
+    {
+        public void Burn()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public void ShowMoveSpeed()
+        {
+
+        }
+    }
+
+    class Human : IMovable
+    {
+        public void Move()
+        {
+
+        }
+
+        public void ShowMoveSpeed()
+        {
 
         }
     }
 }
 
 //Интерфейсы в С#
-//
+//Представляет собой некий набор методов не имеющих реализаций
+//Методы интерфейсов должны быть полностью реализованы в классах наследниках
