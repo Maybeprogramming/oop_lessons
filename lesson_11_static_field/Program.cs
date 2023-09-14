8e0078506a2e16a98395fe2e599d4043be11834d
Maybeonce <a-u-rom@yandex.ru>
2023-09-14 21:01:15 +0400

    !

--- lesson_11_static_field/Program.cs
+++ lesson_11_static_field/Program.cs
@@ -4,6 +4,78 @@
     {
         static void Main()
         {
+            User.Idetifications = 10;
+            User user1 = new User();
+            User user2 = new User();
+            user1.ShowInfo();
+            user2.ShowInfo();
+
+            Console.WriteLine(new string ('-', 30));
+
+            Console.WriteLine("Привет Мир!");
+            MyClass instance = new MyClass();
+            Console.WriteLine(MyClass.StaticField);
+
+            Console.ReadLine();
+        }
+    }
+
+    //Статические члены и классы
+
+    class User
+    {
+        public static int Idetifications;
+        public int Identification;
+
+        public User()
+        {
+            Identification = ++Idetifications;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Identification}");
+        }
+    }
+
+    //Статический конструктор
+    //Техника ленивого инициализации
+    //Т.е. вызываем конструктор в момент первого взаимодействия с типом
+    //Т.е. когда происходит декларирование типа
+    //А вызов обычного конструктора - взаимодействие с типом
+
+    class MyClass
+    {
+        public static float StaticField;
+
+        static MyClass()
+        {
+            StaticField = 10;
+            Console.WriteLine($"Статический конструктор ({StaticField})") ;
+        }
+
+        public MyClass()
+        {
+            Console.WriteLine($"Обычный конструктор");
         }
     }
 }
+
+///СТАТИКА ЭТО ПЛОХО!
+///<summary> 
+/// Задача ООП это:
+/// 1. выделение сущностей
+/// 2. защита их состояния
+/// 3. построение абстракций
+/// и всё это основывается на композиции и взаимосвязанности
+/// 
+/// Всё глобальное можно разбить на композицию объектов
+/// 
+/// При описании типов можно использовать структуры
+/// Схожая с классом но отличается симантикой
+/// Объект класса это сущность и он не равен другому объекту, 
+/// даже если все поля имеют одинаковые значения
+/// 
+/// А объект структуры это объект значения
+/// и он равен другому объекту если значения всех их полей совпадают
+/// </summary>
---
+++ lesson_12_class_and_struct/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace lesson_12_class_and_struct
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    struct Vector2
+    {
+       public int X, Y;
+    }
+}
+
+/// <summary>
+/// У структуры все члены по умолчанию имеют 
+/// публичный модификатор доступа в отличии от класса
+/// </summary>
