c28661a80d75429eebeb953ae80bb3ca712ac9c9
Maybeonce <a-u-rom@yandex.ru>
2023-09-22 12:17:09 +0300

    !

--- lesson_18_events_pattern_practice_04/Program.cs
+++ lesson_18_events_pattern_practice_04/Program.cs
@@ -124,4 +124,63 @@
             Console.WriteLine($"Отправляем SMS сообщение: \r\n{message}\r\n");
         }
     }
+
+    class EventKey
+    {
+
+    }
+
+    class EventSet
+    {
+        private readonly Dictionary<EventKey, Delegate> events = new Dictionary<EventKey, Delegate>();
+
+        public void Add (EventKey eventKey, Delegate handler)
+        {
+            Monitor.Enter(events);
+
+            if (events.TryGetValue(eventKey, out var d))
+            {
+                events[eventKey] = Delegate.Combine(d, handler);
+            }
+            else
+            {
+                events.Add(eventKey, handler);
+            }
+
+            Monitor.Exit(events);
+        }
+
+        public void Remove (EventKey eventKey, Delegate handler)
+        {
+            Monitor.Enter(events);
+
+            if (events.TryGetValue(eventKey, out var d))
+            {
+                d = Delegate.Remove(d, handler);
+            }
+
+            if (d!= null)
+            {
+                events[eventKey] = d;
+            }
+            else
+            {
+                events.Remove(eventKey);
+            }
+
+            Monitor.Exit(events);
+        }
+
+        public void Raise (EventKey eventKey, object sender, EventArgs e)
+        {
+            Monitor.Enter(events);
+
+            if (events.TryGetValue(eventKey, out var d))
+            {
+                d.DynamicInvoke(new object[] { sender, e });
+            }
+
+            Monitor.Exit(events);
+        }
+    }
 }
