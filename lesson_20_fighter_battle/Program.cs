30d0da71b090a6fbb8401acc339d8fc8f6ea622d
Maybeonce <a-u-rom@yandex.ru>
2023-10-03 10:46:25 +0300

    !

--- lesson_20_fighter_battle/Program.cs
+++ lesson_20_fighter_battle/Program.cs
@@ -14,23 +14,24 @@
 
     class BattleField
     {
-        List<Fighter> fighters;
+        private List<Fighter> fighters;
 
         public void StartBattle()
         {
             fighters = new List<Fighter>();
 
-            while (fighters.Count < 5)
+            while (fighters.Count < 2)
             {
                 Console.Clear();
 
-                Console.WriteLine($"Доступные бойцы:\n" +
+                Console.WriteLine(
+                    $"Доступные бойцы:\n" +
                     $"0 - Figter\n" +
                     $"1 - Warrior\n" +
                     $"2 - Assasign\n" +
                     $"3 - Hunter\n" +
                     $"4 - Wizzard\n" +
-                    $"Введите номер для выбора бойца: ");
+                    $"Введите номер для выбора бойца:");
 
                 switch (Console.ReadLine())
                 {
@@ -66,37 +67,64 @@
             }
 
             Console.ReadKey();
-
             StartBattle();
         }
 
-        void ChooseFighter(Fighter fighter) => fighters.Add(fighter);
-        void ChooseFighter(Warrior warrior) => fighters.Add(warrior);
-        void ChooseFighter(Assasign assasign) => fighters.Add(assasign);
-        void ChooseFighter(Hunter hunter) => fighters.Add(hunter);
-        void ChooseFighter(Wizzard wizzard) => fighters.Add(wizzard);
+        private void ChooseFighter(Fighter fighter) => fighters.Add(fighter);
+        private void ChooseFighter(Warrior warrior) => fighters.Add(warrior);
+        private void ChooseFighter(Assasign assasign) => fighters.Add(assasign);
+        private void ChooseFighter(Hunter hunter) => fighters.Add(hunter);
+        private void ChooseFighter(Wizzard wizzard) => fighters.Add(wizzard);
     }
 
     class Fighter : IDamageable, IDamageProvider, IHealable
     {
+        public Fighter()
+        {
+            Health = Generator.GetInt(100, 200);
+            Damage = Generator.GetInt(20, 50);
+            Name = Generator.GetName();
+        }
+
+        public int Health { get; private set; }
+        public int Damage { get; private set; }
+        public bool IsAlive => Health > 0;
+        public string Name { get; private set; }
+
         public void Attack(Fighter target)
         {
-            throw new NotImplementedException();
+            if (target.TryTakeDamage(Damage))
+            {
+                Console.WriteLine($"{GetType().Name} ({Name}) ударил {target.GetType().Name} ({target.Name})");
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} ({Name}) не смог ударить {target.GetType().Name} ({target.Name})");
+            }
         }
 
         public void Healing(int healingPoint)
         {
-            throw new NotImplementedException();
+            Health += healingPoint;
+            Console.WriteLine($"{GetType().Name} ({Name}) подлечил здоровье на ({healingPoint}) ед.");
+
         }
 
-        public void TakeDamage(int damage)
+        public bool TryTakeDamage(int damage)
         {
-            throw new NotImplementedException();
+            if (Health > 0)
+            {
+                Health -= damage;
+                Console.WriteLine($"{GetType().Name} ({Name}) получил удар ({damage}) ед., осталось здоровья ({Health})");
+                return true;
+            }
+
+            return false;
         }
 
         public override string ToString()
         {
-            return $"{this.GetType().Name}";
+            return $"{GetType().Name}";
         }
     }
 
@@ -127,7 +155,7 @@
 
     interface IDamageable
     {
-        void TakeDamage(int damage);
+        bool TryTakeDamage(int damage);
     }
 
     interface IDamageProvider
@@ -138,5 +166,68 @@
     interface IHealable
     {
         void Healing(int healingPoint);
+    }
+
+    static class Generator
+    {
+        private static Random _random = new Random();
+        private static string[] _names =
+            {
+                "Варвар",
+                "Алконафт",
+                "Миледи",
+                "Вульфич",
+                "Страйк",
+                "Герандич",
+                "Фрея",
+                "Крыса",
+                "Нинка",
+                "Царь",
+                "Забота",
+                "Жарить",
+                "Овощ",
+                "Имба",
+                "Нагибатель",
+                "Топчик",
+                "Холивар",
+                "Членчик",
+                "Пирожок",
+                "Котейка",
+                "Оливер",
+                "Викрам",
+                "Архидея",
+                "Вагинометр",
+                "Зимник",
+                "Волкодав",
+                "Богатырь",
+                "Вафлич",
+                "Вурдолакыч",
+                "Зяблик",
+                "Кудахта",
+                "Чувайка",
+                "Мордорка",
+                "Куряха",
+                "Смоляха",
+                "Крендель",
+                "Остряк",
+                "Крушила",
+                "Очкович",
+                "Щавель",
+                "Днище",
+                "Нубичка",
+                "Жираф",
+                "Лизун",
+                "Сосальщик",
+                "Подгузник",
+                "Тряпка"
+            };
+        public static string GetName()
+        {
+            return _names[_random.Next(0, _names.Length - 1)];
+        }
+        public static int GetInt(int minValue, int maxValue)
+        {
+            return _random.Next(minValue, maxValue + 1);
+        }
     }
 }
