98d16e600984e7d53078ff986155709a075a59f1
Maybeonce <a-u-rom@yandex.ru>
2023-10-26 12:31:34 +0300

    !

---
+++ lesson_24_pattertn_abstract_factory/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace lesson_24_pattertn_abstract_factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    abstract class AbstractFactory
+    {
+        public abstract Fighter CreateFighter();
+        public abstract Vihicle CreateVihicle();
+    }
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override Fighter CreateFighter()
+        {
+            return new Sniper();
+        }
+
+        public override Vihicle CreateVihicle()
+        {
+            return new Helicopter();
+        }
+    }
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public override Fighter CreateFighter()
+        {
+            return new Stormtrooper();
+        }
+
+        public override Vihicle CreateVihicle()
+        {
+            return new Tank();
+        }
+    }
+
+    abstract class Fighter
+    { }
+
+    abstract class Vihicle
+    { }
+
+    class Sniper : Fighter
+    { }
+
+    class Helicopter : Vihicle
+    { }
+
+    class Stormtrooper : Fighter
+    { }
+
+    class Tank : Vihicle
+    { }
+}
